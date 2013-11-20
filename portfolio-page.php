409fa8b466cfa112d811c21a849084f6dea50e6b
Sanjay Guruprasad <sanjay@nocturnedevs.com>
2013-11-20 17:24:56 +0530

    Portfolio page styled
    Content serving code completed
    Portfolio page styled
    Responsive design built in

--- portfolio-page.php
+++ portfolio-page.php
@@ -17,15 +17,16 @@ Template Name: Portfolio Page
 		<a href="<?php echo home_url(); ?>/contact-us/">Contact Us</a>
 	</nav>
 
-	<div class="page-section">
+	<div id = "projects" class="page-section">
 	<?php $post_objects = get_field('projects'); ?>
 	<?php if( $post_objects ): ?>
 	    <?php foreach( $post_objects as $post_object): ?>
 	    	<?php $client_data = get_field('client', $post_object->ID); ?>
 	    	<div class="project-container">
 		        <a href="<?php the_permalink($post_object->ID); ?>">
+		        	<img src="<?php echo get_field('client_logo',$client_data->ID); ?>" class="client-logo">
 		        	<h4><?php echo $post_object->post_title; ?></h4>
-		        	<p>Designed for <?php echo $client_data->post_title; ?></p>
+		        	<h5>Designed for <?php echo $client_data->post_title; ?></h5>
 		        	<p><?php the_field('salient_features', $post_object->ID); ?></p>
 		        </a>
 		    </div>
